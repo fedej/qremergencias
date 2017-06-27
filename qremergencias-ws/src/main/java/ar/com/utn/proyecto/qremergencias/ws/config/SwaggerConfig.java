b2fa934eae89239c773c15efe155df519a5da3c5
fedej <federico.jaite@gmail.com>
2017-06-27 17:34:08 -0300

    Swagger Doc para login

--- qremergencias-ws/src/main/java/ar/com/utn/proyecto/qremergencias/ws/config/SwaggerConfig.java
+++ qremergencias-ws/src/main/java/ar/com/utn/proyecto/qremergencias/ws/config/SwaggerConfig.java
@@ -1,15 +1,30 @@
 package ar.com.utn.proyecto.qremergencias.ws.config;
 
+import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import springfox.documentation.builders.OperationBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiDescription;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.ApiListingScannerPlugin;
+import springfox.documentation.spi.service.contexts.DocumentationContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.readers.operation.CachingOperationNameGenerator;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static com.google.common.base.Predicates.and;
 import static com.google.common.base.Predicates.not;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableSwagger2
@@ -20,15 +35,59 @@ public class SwaggerConfig {
 
     @Bean
     // To access the generated swagger
-    // http://localhost:8080/v2/api-docs
+    // http://localhost:8082/qremergencias/v2/api-docs
     // Then it must be copied to src/main/resources/swagger.json
     public Docket swaggerSpringMvcPlugin() {
         return new Docket(DocumentationType.SWAGGER_2)
-            .forCodeGeneration(true)
-            .ignoredParameterTypes(Pageable.class)
-            .select()
-            .paths(and(not(regex("/error.*")),regex("/.*")))
-            .build();
+                .protocols(Collections.singleton("http"))
+                .forCodeGeneration(true)
+                .ignoredParameterTypes(Pageable.class)
+                .select()
+                .paths(and(not(regex("/error.*")), regex("/.*")))
+                .build();
+    }
+
+    @Bean
+    public ApiListingScannerPlugin loginSwaggerDocumentation() {
+        return new LoginSwaggerDocumentation();
+    }
+
+    private static class LoginSwaggerDocumentation implements ApiListingScannerPlugin {
+
+        @Override
+        public List<ApiDescription> apply(final DocumentationContext context) {
+            return new ArrayList<>(Collections.singletonList(new ApiDescription(
+                    "/api/login", "Login Endpoint", Collections.singletonList(
+                    new OperationBuilder(new CachingOperationNameGenerator())
+                            .codegenMethodNameStem("loginUsingPOST")
+                            .consumes(Collections.singleton(APPLICATION_FORM_URLENCODED_VALUE))
+                            .method(HttpMethod.POST)
+                            .tags(Collections.singleton("user-front-controller"))
+                            .parameters(Arrays.asList(new ParameterBuilder()
+                                            .type(new TypeResolver().resolve(String.class))
+                                            .name("username")
+                                            .parameterType("form")
+                                            .parameterAccess("access")
+                                            .required(true)
+                                            .modelRef(new ModelRef("string")) //<5>
+                                            .build(),
+                                    new ParameterBuilder()
+                                            .type(new TypeResolver().resolve(String.class))
+                                            .name("password")
+                                            .parameterType("form")
+                                            .parameterAccess("access")
+                                            .required(true)
+                                            .modelRef(new ModelRef("string")) //<5>
+                                            .build()))
+                            .build()),
+                    false)));
+        }
+
+        @Override
+        public boolean supports(final DocumentationType delimiter) {
+            return DocumentationType.SWAGGER_2.equals(delimiter);
+        }
+
     }
 
 }
